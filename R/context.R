4dc69ed937ec420965ae4235fba7a1759b35c08e
Clément Turbelin <clement.turbelin@gmail.com>
2020-11-06 17:57:29 +0100

    improve docs

--- R/context.R
+++ R/context.R
@@ -1,8 +1,17 @@
 # ResultContext
 
-#' Create a layered context for description
-#' Context is a dynamic list of values, that can be updated by layer
-#' A full context is the merge of all values of all layers
+#' ResultContext Creates a layered set of named values for context description
+#' Context is a dynamic list of named values, that can be updated by layer.
+#' A full context is the merge of all values of all layers, with the rule, last layer overrides the first one.
+#'
+#' Context is used to manage the context of a set of files, typically during the generation of them (you know well the context of a file when you create it)
+#'
+#' For example, during data analysis, a  high number of files can be generated and some can share the same context (same output, same analysis method, same step, ...)
+#' The context can be used to manage this shared context, an create a dedicated context for each file or group of file.
+#' The layered feature allow to restore a previous context or to work in a recursive way (using push() and pop() to manage recursion level)
+#'
+#' The resolved context is the set of labels in the current context with all layers merged. You can see the context as the "labels" in K8s,, a label has a name and a value.
+#'
 #' A layer can be added (push) or removed (pop)
 #'
 #' @examples
@@ -10,7 +19,7 @@
 #' context$set(step="main")
 #' context$push() # Create a new layer
 #' context$set(level=1)
-#' context$resolve()
+#' context$resolve() # get the values for all merged layers
 #' context$pop() # Get back to first layer
 #' context$resolve()
 #' @export
